fde64d1310f954c384ed234a039ddd25889d9ae2
svr2kos2 <svr2kos2@hotmail.com>
2023-06-13 20:12:15 +0800

    release camera from cinamachine

--- GenshinImpactVR/Utility.cs
+++ GenshinImpactVR/Utility.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Cinemachine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,8 @@ using System.Threading.Tasks;
 using UnityEngine;
 
 namespace GenshinImpactVR {
-    internal class Utility {
-        public void RenderTest() {
+    public class Utility {
+        public static Camera RenderTest() {
             string[] destroyList = { "VRPlayer", "RawImage", "RawImageBackground" };
             foreach (string destroy in destroyList) {
                 var obj = GameObject.Find(destroy);
@@ -15,17 +16,14 @@ namespace GenshinImpactVR {
                     UnityEngine.Object.DestroyImmediate(obj);
                 }
             }
+            Camera.main.GetComponent<Cinemachine.CinemachineExternalCamera>().enabled = false;
             var canvas = GameObject.Find("Pages").transform.parent.gameObject;
-            //var background = new GameObject("RawImageBackground");
-            //background.transform.SetParent(canvas.transform);
-            //var backGroundImage = background.AddComponent<UnityEngine.UI.RawImage>();
-            //backGroundImage.color = new Color(1, 1, 1, 1);
-            //var backgroundRT = background.GetComponent<RectTransform>();
-            //backgroundRT.localScale = Vector3.one;
-            //backgroundRT.pivot = Vector2.zero;
-            //backgroundRT.anchorMin = Vector2.zero;
-            //backgroundRT.anchorMax = Vector2.zero;
-            //backgroundRT.sizeDelta = new Vector2(1920/2, 1200/2);
+            var player = UnityEngine.Object.Instantiate(Camera.main.gameObject);
+            //player.transform.SetParent(Camera.main.transform);
+            player.name = "VRPlayer";
+            player.transform.position = Camera.main.transform.position;
+            player.transform.rotation = Camera.main.transform.rotation;
+            var cam = player.GetComponent<Camera>();
 
             var rawImage = new GameObject("RawImage");
             rawImage.transform.SetParent(canvas.transform);
@@ -35,29 +33,17 @@ namespace GenshinImpactVR {
             rawImageRectTransform.pivot = Vector2.zero;
             rawImageRectTransform.anchorMin = Vector2.zero;
             rawImageRectTransform.anchorMax = Vector2.zero;
-            rawImageRectTransform.sizeDelta = new Vector2(1920 / 2, 1200 / 2);
+            rawImageRectTransform.sizeDelta = new Vector2(cam.pixelWidth / 2, cam.pixelHeight / 2);
 
-            var player = UnityEngine.Object.Instantiate(Camera.main.gameObject);
-            player.transform.SetParent(Camera.main.transform);
-            player.name = "VRPlayer";
-            player.transform.localPosition = Vector3.zero;
-            player.transform.localRotation = Quaternion.identity;
-
-            //var player = new GameObject("VRPlayer");
-            //player.transform.parent = Camera.main.transform;
-            //player.transform.localPosition = Vector3.zero;
-            //player.transform.localRotation = Quaternion.identity;
-            var cam = player.GetComponent<Camera>();
             var rt = new RenderTexture(cam.pixelWidth, cam.pixelHeight, 24, RenderTextureFormat.RGB111110Float);
-
-
             var camPost = cam.gameObject.GetComponent<UnityEngine.Rendering.PostProcessing.PostProcessLayer>();
             camPost.finalTarget = rt;
             camPost.isExtraCamera = true;
-            //camPost.m_ScreenHeight = cam.pixelWidth;
-            //camPost.m_ScreenWidth = cam.pixelHeight;
-
             rawImage.GetComponent<UnityEngine.UI.RawImage>().texture = rt;
+            GameObject.Destroy(cam.GetComponent<Cinemachine.CinemachineExternalCamera>());
+            GameObject.Destroy(cam.GetComponent<Cinemachine.CinemachineBrain>());
+            cam.fieldOfView = 90f;
+            return cam;
         }
     }
 }
